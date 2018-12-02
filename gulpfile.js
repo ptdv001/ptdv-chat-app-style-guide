e594538c110d21038a366eddf681e685a5e3fe15
ptdv001 <peterdev001@hotmail.com>
2018-12-01 17:47:10 -0800

    Adds fontawesome core dependency

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,7 @@
+// TODO
+// use rollup to package module see: https://medium.com/@backspaces/es6-modules-part-1-migration-strategy-a48de0b7f112
+// consider using NPM instead for build (vs Gulp)
+
 const gulp = require('gulp');
 const clean = require('gulp-clean');
 const runSequence = require('run-sequence');
@@ -5,13 +9,14 @@ const concat = require('gulp-concat');
 
 const SRC_PATH = 'src/icons';
 const DIST_PATH = 'dist';
-const ICONS_IN_FILE = 'icons-custom.js';
+const ICONS_IN_FILE = 'ptdv-fontawesome.js';
 const ICONS_OUT_FILE = 'ptdv-fontawesome.js';
 
+// Note: for now just include fontawesome and add custom icons in project
 const FONTAWESOME_FILES = [
-  'node_modules/@fortawesome/fontawesome/index.js',
-  'node_modules/@fortawesome/fontawesome-free-solid/index.js',
-  'node_modules/@fortawesome/fontawesome-free-brands/index.js',
+  // 'node_modules/@fortawesome/fontawesome/index.js',
+  // 'node_modules/@fortawesome/fontawesome-free-solid/index.js',
+  // 'node_modules/@fortawesome/fontawesome-free-brands/index.js',
   SRC_PATH + '/' + ICONS_IN_FILE
 ];
 
